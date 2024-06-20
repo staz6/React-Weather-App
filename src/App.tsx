d97f4d785591673a791a83e5e7e27cf40999dedb
staz6 <61322266+staz6@users.noreply.github.com>
2024-06-20 17:05:47 +0500

    feat: add rules for eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from "../../../../../vite.svg"
 import './App.css'
 
-function App() {
+function App():JSX.Element {
   const [count, setCount] = useState(0)
 
   return (
@@ -18,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type='submit' onClick={() => setCount((prevcount) => prevcount + 1)}>
           count is {count}
         </button>
         <p>
