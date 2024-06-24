70632eaca35908f700c6f8268ede0a8d8d41def0
staz6 <61322266+staz6@users.noreply.github.com>
2024-06-24 20:02:04 +0500

    feat: make CurrentWeatherCard partially  dynamic
    Ensure real-time temperature display for searched city. Date and time updates in next commit.

--- src/Components/Compound/CurrentWeatherCard.tsx
+++ src/Components/Compound/CurrentWeatherCard.tsx
@@ -1,28 +1,72 @@
 import React from "react";
-import StaticWeatherImg from "../../assets/WeatherIcon.png";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useWeatherContext } from "../../Context/WeatherContext";
 
-const CurrentWeatherCard: React.FC = () => (
-  <div className="px-md-10 px-5 2xl:px-12 ">
-    <img
-      className="w-auto h-[180px] object-contain"
-      src={StaticWeatherImg}
-      alt=""
-    />
+interface WeatherData {
+  main: {
+    temp: number;
+  };
+  dt: number;
+  weather: {
+    icon: string;
+  }[];
+}
 
-    <div className="text-white pl-5 font-sans">
-      <h1 className="text-9xl leading-none relative top-temp-top font-extralight mb-3">
-        27.2
-        <sup className="text-3xl font-normal top-custom-super pl-4 align-super">
-          °C
-        </sup>
-      </h1>
+const API_KEY = "a611ea741ba0588548e2d0e39313050b";
 
-      <h2 className="text-3xl font-light mb-1">17th Jun &apos;21</h2>
-      <h3 className="text-xl font-light tracking-wide">
-        Thursday <span className="mx-2">|</span> 4:52 PM
-      </h3>
+const convertKelvinToCelsius = (
+  kelvin: number | undefined,
+): number | undefined => (kelvin !== undefined ? kelvin - 273.15 : undefined);
+
+const CurrentWeatherCard: React.FC = () => {
+  const { searchCity } = useWeatherContext();
+
+  const postQuery = useQuery<WeatherData>({
+    queryKey: ["weather", searchCity],
+    queryFn: async () => {
+      try {
+        const response = await axios.get<WeatherData>( // Specify the response type
+          `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${API_KEY}`,
+        );
+        return response.data;
+      } catch (error) {
+        throw new Error("Error fetching data");
+      }
+    },
+    enabled: !!searchCity,
+  });
+
+  if (postQuery.isLoading) return <h1>Loading....</h1>;
+  if (postQuery.isError) return <h1>No Such City Exist</h1>;
+  if (!searchCity) {
+    return <h1>No city searched</h1>;
+  }
+
+  if (postQuery.data !== undefined) {
+    // console.log(postQuery.data);
+  }
+  const iconUrl = `http://openweathermap.org/img/wn/${postQuery.data?.weather[0].icon}@2x.png`;
+
+  return (
+    <div className="px-md-10 px-5 2xl:px-12 ">
+      <img className="w-auto h-[180px] object-contain" src={iconUrl} alt="" />
+
+      <div className="text-white pl-5 font-sans">
+        <h1 className="text-9xl leading-none relative top-temp-top font-extralight mb-3">
+          {convertKelvinToCelsius(postQuery.data?.main.temp)?.toFixed()}
+          <sup className="text-3xl font-normal top-custom-super pl-4 align-super">
+            °C
+          </sup>
+        </h1>
+
+        <h2 className="text-3xl font-light mb-1">17th Jun &apos;21</h2>
+        <h3 className="text-xl font-light tracking-wide">
+          Thursday <span className="mx-2">|</span> 4:52 PM
+        </h3>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CurrentWeatherCard;
