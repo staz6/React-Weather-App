085f0ed1a46b89cab6daabf6b4f5cd34b3416bfc
staz6 <61322266+staz6@users.noreply.github.com>
2024-06-27 20:24:41 +0500

    refactor: move calculateWeathermertic to helper.ts

--- src/Components/Compound/WeahterMetricsCard.tsx
+++ src/Components/Compound/WeahterMetricsCard.tsx
@@ -1,37 +1,25 @@
 import React from "react";
-import { BiWind } from "react-icons/bi";
-import { BsCloudRainHeavy } from "react-icons/bs";
-import { WiHumidity } from "react-icons/wi";
 import { useWeatherContext } from "../../Context/WeatherContext";
+import { calculateWeatherMetrics } from "../../HelperFunctions/Helper";
 
 const WeatherMetricsCard: React.FC = () => {
   const { currentWeatherData } = useWeatherContext();
-  const rain = currentWeatherData?.rain?.["1h"];
-  const wind = currentWeatherData?.wind.speed;
-  const hum = currentWeatherData?.main.humidity;
 
+  const metrics = calculateWeatherMetrics(currentWeatherData);
   return (
-    <div className="flex  items-center m-auto justify-center mt-10 sm:gap-8 gap-3 text-gray-200 text-sm sm:text-md">
-      <div className="flex items-center sm:gap-2 gap-1">
-        <BiWind className="sm:text-2xl text-lg" color="white" />
-        <h3>
-          <span className="sm:mr-2">{wind}</span> km/h
-        </h3>
-      </div>
-      |
-      <div className="flex items-center sm:gap-2 gap-1">
-        <WiHumidity className="sm:text-2xl text-lg" color="white" />
-        <h3>
-          <span className="sm:mr-2">{hum}</span> %
-        </h3>
-      </div>
-      |
-      <div className="flex items-center sm:gap-2 gap-1">
-        <BsCloudRainHeavy className="sm:text-2xl text-lg" color="white" />
-        <h3>
-          <span className="sm:mr-2">{rain}</span> mm
-        </h3>
-      </div>
+    <div className="flex  items-center m-auto justify-center mt-10 sm:gap-8 gap-5 text-gray-200 text-sm sm:text-md">
+      {metrics.map((Item, index) => (
+        <>
+          <div key={index} className="flex items-center sm:gap-2 gap-2">
+            <Item.Icon className="sm:text-4xl text-xl" color="white" />
+            <h3>
+              <span className="sm:mr-2 sm:text-xl">{Item.value}</span>{" "}
+              <span className="sm:text-xl">{Item.unit}</span>
+            </h3>
+          </div>{" "}
+          {index !== 2 && "|"}
+        </>
+      ))}
     </div>
   );
 };
--- src/HelperFunctions/Helper.ts
+++ src/HelperFunctions/Helper.ts
@@ -1,3 +1,7 @@
+import { BiWind } from "react-icons/bi";
+import { WiHumidity } from "react-icons/wi";
+import { BsCloudRainHeavy } from "react-icons/bs";
+
 export default function convertKelvinToCelsius(
   kelvin: number | undefined,
 ): number | undefined {
@@ -55,3 +59,45 @@ export const suneventtimes = (
     { name: "Sunset", ampm: sunsetAMPM, militaryTime: sunset24Hour },
   ];
 };
+
+interface Metric {
+  Icon: React.ElementType;
+  value: number | undefined;
+  unit: string;
+}
+
+interface WeatherData {
+  rain?: { "1h": number };
+  wind: { speed: number };
+  main: { humidity: number };
+}
+
+const calculateWeatherMetrics = (
+  data: WeatherData | undefined | null,
+): Metric[] => {
+  const rainMetric = data?.rain?.["1h"];
+  const windMetric = data?.wind.speed ?? 0;
+  const humidityMetric = data?.main.humidity ?? 0;
+
+  const metrics: Metric[] = [
+    {
+      Icon: BiWind,
+      value: windMetric,
+      unit: "km/h",
+    },
+    {
+      Icon: WiHumidity,
+      value: humidityMetric,
+      unit: "%",
+    },
+    {
+      Icon: BsCloudRainHeavy,
+      value: rainMetric,
+      unit: "mm",
+    },
+  ];
+
+  return metrics;
+};
+
+export { calculateWeatherMetrics };
