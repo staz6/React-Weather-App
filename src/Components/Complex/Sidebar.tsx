d0ec57d86b33c8e903bb7dd4f1d4c1ab643330ff
staz6 <61322266+staz6@users.noreply.github.com>
2024-07-05 16:05:57 +0500

    feat: added react lazay and suspense in currentweathercard

--- src/Components/Complex/Sidebar.tsx
+++ src/Components/Complex/Sidebar.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import Searchbar from "../Compound/Searchbar";
-import CurrentWeatherInfo from "../Compound/CurrentWeatherCard";
 import TemperatureChart from "../Compound/TemperatureChart";
+
+const CurrentWeatherInfo = lazy(() => import("../Compound/CurrentWeatherCard"));
 
 const Sidebar: React.FC = () => (
   <div className="border-r border-custom-gray flex flex-col bg-linearSide pb-10 lg:pb-0  w-full lg:w-1/3 p-7 lg:p-4 xl:p-7 space-y-0 overflow-x-hidden">
     <Searchbar />
     <div className="flex justify-center gap flex-col sm:flex-row items-center lg:flex-col">
-      <CurrentWeatherInfo />
+      <Suspense fallback={<p>Loading...</p>}>
+        <CurrentWeatherInfo />
+      </Suspense>
       <TemperatureChart />
     </div>
   </div>
