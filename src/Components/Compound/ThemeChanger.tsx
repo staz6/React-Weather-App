b233030abced7731f4033cc96c12d66f88cc9d3f
staz6 <61322266+staz6@users.noreply.github.com>
2024-07-11 19:41:01 +0500

    refactor: improved themechanger semantics

--- src/Components/Compound/ThemeChanger.tsx
+++ src/Components/Compound/ThemeChanger.tsx
@@ -9,24 +9,20 @@ const ThemeChanger: React.FC = () => {
   return (
     <Button
       icon=""
-      className="fixed bottom-3 z-50 w-20 border rounded-2xl"
+      className="fixed bottom-3 flex justify-between z-50 w-20 border rounded-2xl"
       description=""
       onClick={toggleDarkTheme}
     >
-      <div className="flex justify-between">
-        <Button
-          className="dark:bg-transparent bg-yellow-300 w-10 transition duration-300  rounded-2xl"
-          onClick={() => null}
-          icon={<BiSun className="m-auto" color="white" size={24} />}
-          description=""
-        />
-        <Button
-          className="dark:bg-black bg-transparent w-10 transition duration-300  rounded-2xl"
-          onClick={() => null}
-          icon={<FaMoon className="m-auto" color="white" size={24} />}
-          description=""
-        />
-      </div>
+      <BiSun
+        className="m-auto   dark:bg-transparent bg-yellow-300 w-10 transition duration-300  rounded-2xl"
+        color="white"
+        size={24}
+      />
+      <FaMoon
+        className="m-auto dark:bg-black bg-transparent w-10 transition duration-300  rounded-2xl"
+        color="white"
+        size={21}
+      />
     </Button>
   );
 };
