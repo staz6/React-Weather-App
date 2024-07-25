45d7fa035b6b0bb5d8bf6a2bdf06f057ec9b5c46
staz6 <61322266+staz6@users.noreply.github.com>
2024-07-25 15:58:38 +0500

    test: tested lazySuneventcard component

---
+++ src/Components/lazyloaded/__test__/LazySuneventCard.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import LazySuneventCard from "../LazySuneventCard";
+
+describe("LazySuneventCard Component Tests", () => {
+  test("testing rendering of UI elements", () => {
+    render(<LazySuneventCard />);
+    const sunrise = screen.getByText("Sunrise");
+    expect(sunrise).toBeInTheDocument();
+    const sunset = screen.getByText("Sunset");
+    expect(sunset).toBeInTheDocument();
+    const goldenHour = screen.getByText("Golden Hour");
+    expect(goldenHour).toBeInTheDocument();
+  });
+});
