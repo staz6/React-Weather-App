6ec0dff7aec2c8b5373b9a637acbf4802d7bb794
staz6 <61322266+staz6@users.noreply.github.com>
2024-07-26 21:22:54 +0500

    test: added addTofavorite and themechanger feature testcases

--- cypress/e2e/WeatherAppTest.cy.ts
+++ cypress/e2e/WeatherAppTest.cy.ts
@@ -1,3 +1,35 @@
+const searchCityUsingInput = (): void => {
+  cy.visit("http://localhost:5173/");
+  const InputButton = cy
+    .get('[data-testid="searchcity_input"]')
+    .should("exist");
+  cy.get('[data-testid="location_btn"]').should("exist");
+  InputButton.click();
+  const SearchInput = cy.get('[placeholder="Enter City"]');
+  SearchInput.type("California");
+  InputButton.click();
+};
+
+const useThemeChanger = (): void => {
+  const ThemeChangerBtn = cy
+    .get('[data-testid="ThemeChanger"]')
+    .should("exist");
+  cy.get("html").should("not.have.class", "dark");
+  ThemeChangerBtn.click();
+  cy.get("html").should("have.class", "dark");
+  ThemeChangerBtn.click();
+  cy.get("html").should("not.have.class", "dark");
+};
+
+const useAddToFav = (): void => {
+  const AddToFavBtn = cy.get('[data-testid="FavCity"]').should("exist");
+  AddToFavBtn.click();
+  const InputButton = cy
+    .get('[data-testid="searchcity_input"]')
+    .should("exist");
+  InputButton.click();
+};
+
 describe("Weather App End-2-End testing", () => {
   it("Displaying ui on initial page load", () => {
     cy.visit("http://localhost:5173/");
@@ -19,16 +51,8 @@ describe("Weather App End-2-End testing", () => {
   });
   it("Searching city using input searchbar and testing website behaviour from user perspective", () => {
     cy.visit("http://localhost:5173/");
-    const InputButton = cy
-      .get('[data-testid="searchcity_input"]')
-      .should("exist");
-
-    cy.get('[data-testid="location_btn"]').should("exist");
-    InputButton.click();
-    const SearchInput = cy.get('[placeholder="Enter City"]');
     cy.get('[data-testid="ThemeChanger"]').should("exist");
-    SearchInput.type("California");
-    InputButton.click();
+    searchCityUsingInput();
     cy.contains("Loading CurrentWeather").should("exist");
     cy.contains("Loading Temperature Chart").should("exist");
     cy.contains("Loading ").should("exist");
@@ -40,5 +64,12 @@ describe("Weather App End-2-End testing", () => {
     cy.get('[data-testid="sunevent"]').should("exist");
     cy.get('[data-testid="WeatherMetricsCard"]').should("exist");
     cy.get('[data-testid="WeatherForecastItem"]').should("exist");
+  });
+
+  it("using addTofavorite feature and themechanger", () => {
+    searchCityUsingInput();
+    useAddToFav();
+    cy.contains("1. California");
+    useThemeChanger();
   });
 });
