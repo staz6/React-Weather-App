008af0d9361fc95f004f17a2a3fdc2c6c4a9c5b1
staz6 <61322266+staz6@users.noreply.github.com>
2024-07-29 18:45:28 +0500

    refactor: improve test cases and include error checking

--- cypress/e2e/WeatherAppTest.cy.ts
+++ cypress/e2e/WeatherAppTest.cy.ts
@@ -2,24 +2,15 @@ const searchCityUsingInput = (city: string): void => {
   const SearchInput = cy.get("input.SearchInput");
   SearchInput.clear().type(city);
 };
-const useNotification = (): void => {
-  const notificationBtn = cy.get('[data-testid="bell_btn"]').should("exist");
-  notificationBtn.click();
+const useCurrenLocation = (): void => {
+  cy.setGeolocation(37.7749, -122.4194);
+  const CurrentLocationBtn = cy
+    .get('[data-testid="location_btn"]')
+    .should("exist");
+  CurrentLocationBtn.click();
+  cy.contains("San Francisco");
 };
-const showWeatherResults = (): void => {
-  cy.get('[data-testid="WeatherIcon"]').should("exist");
-  cy.get('[data-testid="temperature"]').should("exist");
-  cy.get('[data-testid="TemperatureChart"]').should("exist");
-  cy.get('[data-testid="sunevent"]').should("exist");
-  cy.get('[data-testid="WeatherMetricsCard"]').should("exist");
-  cy.get('[data-testid="WeatherForecastItem"]').should("exist");
-};
-const useLazyloading = (visibility: string): void => {
-  cy.contains("Loading CurrentWeather").should(visibility);
-  cy.contains("Loading Temperature Chart").should(visibility);
-  cy.contains("Loading ").should(visibility);
-  cy.contains("loading weather metrics").should(visibility);
-  cy.contains("Loading Forecast").should(visibility);
+const showWeatherResults = (visibility: string): void => {
   cy.get('[data-testid="WeatherIcon"]').should(visibility);
   cy.get('[data-testid="temperature"]').should(visibility);
   cy.get('[data-testid="TemperatureChart"]').should(visibility);
@@ -27,16 +18,19 @@ const useLazyloading = (visibility: string): void => {
   cy.get('[data-testid="WeatherMetricsCard"]').should(visibility);
   cy.get('[data-testid="WeatherForecastItem"]').should(visibility);
 };
+const useLazyloading = (visibility: string): void => {
+  cy.contains("Loading CurrentWeather").should(visibility);
+  cy.contains("Loading Temperature Chart").should(visibility);
+  cy.contains("Loading ").should(visibility);
+  cy.contains("loading weather metrics").should(visibility);
+  cy.contains("Loading Forecast").should(visibility);
+};
 
 const useThemeChanger = (): void => {
   const ThemeChangerBtn = cy
     .get('[data-testid="ThemeChanger"]')
     .should("exist");
-  cy.get("html").should("not.have.class", "dark");
   ThemeChangerBtn.click();
-  cy.get("html").should("have.class", "dark");
-  ThemeChangerBtn.click();
-  cy.get("html").should("not.have.class", "dark");
 };
 
 const ClickInputButton = (): void => {
@@ -44,6 +38,11 @@ const ClickInputButton = (): void => {
     .get('[data-testid="searchcity_input"]')
     .should("exist");
   InputButton.click();
+};
+
+const useNotification = (): void => {
+  const notificationBtn = cy.get('[data-testid="bell_btn"]').should("exist");
+  notificationBtn.click();
 };
 
 const useAddToFav = (): void => {
@@ -80,48 +79,22 @@ const useSelectForecastDay = (): void => {
         });
     });
 };
-describe("Weather App End-2-End testing", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:5173/");
-  });
-  it("Displaying ui on initial page load", () => {
-    cy.contains("No City Searched").should("exist");
-    useNotification();
-    useLazyloading("not.exist");
-  });
-  it("Searching city using input searchbar and testing website behaviour from user perspective", () => {
-    cy.get('[data-testid="ThemeChanger"]').should("exist");
-    cy.get('[data-testid="location_btn"]').should("exist");
-    ClickInputButton();
-    searchCityUsingInput("California");
-    ClickInputButton();
-    useLazyloading("exist");
-    showWeatherResults();
-  });
 
-  it("using addTofavorite feature and themechanger", () => {
-    ClickInputButton();
-    searchCityUsingInput("California");
-    ClickInputButton();
-    useAddToFav();
-    ClickInputButton();
-    cy.contains("1. California");
-    searchCityUsingInput("Karachi");
-    ClickInputButton();
-    useAddToFav();
-    ClickInputButton();
-    cy.get('[data-testid="favcitylist"]').first().click();
-    ClickInputButton();
-    useThemeChanger();
-  });
-  it("using display selected day's forecast data on selecting day ", () => {
-    ClickInputButton();
-    searchCityUsingInput("California");
-    ClickInputButton();
-    cy.scrollTo("bottom");
-    useSelectForecastDay();
-  });
-  it("Using current location and testing all features", () => {
+const closeAutoNotifications = (): void => {
+  const notificationBtn = cy
+    .get('[data-testid="auto_notify_Btn"]')
+    .should("exist");
+  notificationBtn.click({ multiple: true });
+};
+const closeManualNotifications = (): void => {
+  const notificationBtn = cy
+    .get('[data-testid="manual_notify_Btn"]')
+    .should("exist");
+  notificationBtn.click();
+};
+
+describe("Weather App End-2-End testing", () => {
+  it("Testing website functionality and features ", () => {
     cy.intercept("GET", "http://api.weatherapi.com/v1/forecast.json*", {
       statusCode: 200,
       body: {
@@ -141,19 +114,49 @@ describe("Weather App End-2-End testing", () => {
         },
       },
     }).as("getWeatherAlerts");
-    cy.setGeolocation(37.7749, -122.4194);
-    const CurrentLocationBtn = cy
-      .get('[data-testid="location_btn"]')
-      .should("exist");
-    CurrentLocationBtn.click();
-    useLazyloading("exist");
-    cy.get('[data-testid="auto_notify"]').should("exist");
-    cy.get('[data-testid="auto_notify_Btn"]')
-      .should("exist")
-      .click({ multiple: true });
-    showWeatherResults();
+
+    cy.visit("http://localhost:5173/");
+    showWeatherResults("not.exist");
+    useLazyloading("not.exist");
     useNotification();
-    cy.get('[data-testid="manual_notify"]').should("exist");
-    cy.get('[data-testid="manual_notify_Btn"]').should("exist").click();
+    cy.contains("No notifications");
+    ClickInputButton();
+    searchCityUsingInput("Berlin");
+    ClickInputButton();
+    useLazyloading("exist");
+    showWeatherResults("exist");
+    useAddToFav();
+    closeAutoNotifications();
+    useThemeChanger();
+    cy.get("html").should("have.class", "dark");
+    cy.get("html").should("have.class", "dark");
+    cy.wait(500);
+    cy.scrollTo("bottom");
+    useSelectForecastDay();
+    cy.scrollTo("top");
+    useCurrenLocation();
+    useLazyloading("exist");
+    showWeatherResults("exist");
+    closeAutoNotifications();
+    useNotification();
+    closeManualNotifications();
+    ClickInputButton();
+    cy.get('[data-testid="favcitylist"]').first().click();
+    ClickInputButton();
+    closeAutoNotifications();
+    showWeatherResults("exist");
+    useThemeChanger();
+    cy.get("html").should("not.have.class", "dark");
+  });
+  it("Testing errors", () => {
+    cy.visit("http://localhost:5173/");
+    cy.on("uncaught:exception", (): boolean => false);
+    ClickInputButton();
+    searchCityUsingInput("fdsafdaf");
+    ClickInputButton();
+    useLazyloading("exist");
+    showWeatherResults("not.exist");
+    cy.wait(8000);
+    cy.contains("Please Make Sure Such City Exists");
   });
 });
