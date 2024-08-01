d6dcb2917d44d27da3151459b7689b53581ae1c2
staz6 <61322266+staz6@users.noreply.github.com>
2024-08-01 22:20:55 +0500

    feat: added button component story

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { BsTrophyFill } from "react-icons/bs";
+import Button from "../Components/Shared/Button"; // Adjust path if needed
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    // eslint-disable-next-line no-alert
+    onClick: () => alert("Button clicked!"),
+    icon: <BsTrophyFill />,
+    className: "bg-red-500  p-2 flex place-items-center gap-2", // Set className here
+    description: "click",
+    children: "me",
+    testid: "primary-button",
+  },
+};
