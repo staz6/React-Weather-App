5b37c7089fac9a3b803ed49db9817a95bed4350d
Fozan3060 <61322266+staz6@users.noreply.github.com>
2024-08-13 00:39:18 +0500

    chore: update styles of forecastitem

--- src/Components/Compound/WeatherForecastItem.tsx
+++ src/Components/Compound/WeatherForecastItem.tsx
@@ -15,27 +15,29 @@ interface Props {
 const WeatherForecastItem: React.FC<Props> = ({ item, animation = true }) => {
   const { settimeStamp, timeStamp } = useWeatherContext();
   return (
-    <div
-      data-testid="WeatherForecastItem"
-      role="button"
-      tabIndex={0}
-      onKeyDown={(e) => {
-        if (e.key === "Enter" || e.key === " ") {
-          settimeStamp(item.timeStamp);
+    <div className="linearBorder">
+      <div
+        data-testid="WeatherForecastItem"
+        role="button"
+        tabIndex={0}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" || e.key === " ") {
+            settimeStamp(item.timeStamp);
+          }
+        }}
+        onClick={() =>
+          timeStamp === item.timeStamp
+            ? (settimeStamp(""), action("removed timestamp")())
+            : (settimeStamp(item.timeStamp), action("added timestamp")())
         }
-      }}
-      onClick={() =>
-        timeStamp === item.timeStamp
-          ? (settimeStamp(""), action("removed timestamp")())
-          : (settimeStamp(item.timeStamp), action("added timestamp")())
-      }
-      className={`cursor-pointer text-shadow-CustomShadow  dark:bg-linearSideDark  bg-linearSide w-36 m-auto sm:m-0 sm:w-fit ${animation ? "hover:-translate-y-3 duration-200" : ""}  shadow-lg  text-center py-4 rounded-xl`}
-    >
-      <h1 data-testid="forecast_temp" className="text-lg">
-        {item.temp} °C
-      </h1>
-      <img src={item.icon} className="w-24 h-24 m-auto" alt="" />
-      <h1 className="tracking-wider text-lg font-light">{item.day}</h1>
+        className={`cursor-pointer border-t border-r text-shadow-CustomShadow bg-white bg-opacity-20 w-36 m-auto sm:m-0 sm:w-fit ${animation ? "hover:-translate-y-3 duration-200" : ""}  shadow-lg  text-center py-4 rounded-xl`}
+      >
+        <h1 data-testid="forecast_temp" className="text-lg">
+          {item.temp} °C
+        </h1>
+        <img src={item.icon} className="w-[6rem] h-[5.5rem] m-auto" alt="" />
+        <h1 className="tracking-wider text-lg font-light">{item.day}</h1>
+      </div>
     </div>
   );
 };
